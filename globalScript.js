3ae46ba3bfb39faf35965a52dfa8163ad027093f
kaviraj022 <ikavikr8098@gmail.com>
2024-12-20 07:54:39 +0530

    Added a 'move to top' button dynamically Completed the structure of all the pages Added the extra page for internships

---
+++ globalScript.js
@@ -0,0 +1,25 @@
+// Create the "Move to Top" button dynamically for all pages
+let moveToTopBtn = document.createElement("button");
+moveToTopBtn.id = "moveToTopBtn";
+moveToTopBtn.title = "Go to top";
+moveToTopBtn.innerHTML = "↑"; // Button text (upward arrow)
+
+// Add the button to the body
+document.body.appendChild(moveToTopBtn);
+
+// Show the button when the user scrolls down 200px
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        moveToTopBtn.style.display = "block";
+    } else {
+        moveToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the page
+moveToTopBtn.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
